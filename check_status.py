a18cddc94c76fea281a8bb6cbe04c665a0692e66
Beatriz Galiana Carballido <beagaliana@riseup.net>
2019-12-30 14:32:07 +0100

    Create tests for functions
    Simple tests that are called in main to check functions are working
    properly.

--- check_status.py
+++ check_status.py
@@ -1,5 +1,7 @@
 import pycurl, validators
-
+import urllib.request
+import sys
+import socket
 
 def url_exists(url):
 	
@@ -24,11 +26,48 @@ def url_exists(url):
 	else:
 		raise ValueError('"{}" is not a valid url'.format(url))
 
+# Check status using urllib
+def get_status_code(url):
+    return urllib.request.urlopen(url).getcode()
 
-print("WEB CONNECTIVITY CHECKER TOOL")
-host = input('Host: ')
+# Check if socket on the server is listening
+def check_socket_server(host, port):
+    addr_info = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+    for family, socktype, proto, canonname, sockaddr in addr_info:
+        s = socket.socket(family, socktype, proto)
+        try:
+            s.connect(sockaddr)
+        except socket.error:
+            return False
+        else:
+            s.close()
+            return True
 
-if (url_exists("http://"+host)):
-	print("CONNECTION SUCCESSFUL: website up.")
-else:
-	print("CONNECTION FAILED: website down.")
+def test_url_exists():
+    print("WEB CONNECTIVITY CHECKER TOOL")
+    host = input('Host: ')
+
+    if (url_exists("http://"+host)):
+        print("CONNECTION SUCCESSFUL: website up.")
+    else:
+        print("CONNECTION FAILED: website down.")
+
+def test_connection_urllib():
+    url = str(input('Type the site you want to check: '))
+    print(get_status_code(url))
+
+def test_server_socket_connection():
+    host = input('Host: ')
+    port = int(input('Port: '))
+    if check_socket_server(host, port):
+        print("Server is UP")
+    else:
+        print("Server is DOWN")
+
+def main():
+#    test_connection_urllib()
+#    test_server_socket_connection
+    test_url_exists()
+
+if __name__=='__main__':
+    main()
